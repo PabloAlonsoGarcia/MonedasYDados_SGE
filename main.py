8b68611751b09996e17b39f9413c362f8690dfbb
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2022-10-18 10:25:18 +0200

    Tiradas y diccionario hechos

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ diccionario = {
     },
     'dado': {}
 }
-
 if __name__ == '__main__':
 
-    for x in 1_000_000:
+    for y in range(11):
+        diccionario['moneda']['cara'][y] = 0
+        diccionario['moneda']['cruz'][y] = 0
+    for y in range(3, 19):
+        diccionario['dado'][y] = 0
+
+
+
+    for x in range(1_000_000):
         contCara = 0
         conCruz = 0
-        for y in 10:
+        valorDados=0
+        for y in range(10):
             nRandom = random.randint(0, 1)
             if nRandom == 0:
                 contCara += 1
@@ -23,9 +31,12 @@ if __name__ == '__main__':
         diccionario['moneda']['cara'][contCara] += 1
         diccionario['moneda']['cruz'][conCruz] += 1
 
-        for z in 3:
-            zRandom
+        for z in range(3):
+            zRandom = 0
+            valorDados += random.randint(1, 6)
+        diccionario['dado'][valorDados] += 1
 
+    pprint(diccionario)
 
 
 
